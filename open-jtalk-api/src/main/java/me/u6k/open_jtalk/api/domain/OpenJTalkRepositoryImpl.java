d86260957cf1599ede41dbe5262b791067a5d8fd
kng <masan580@gmail.com>
2018-06-12 13:28:06 +0900

    change cmd

--- open-jtalk-api/src/main/java/me/u6k/open_jtalk/api/domain/OpenJTalkRepositoryImpl.java
+++ open-jtalk-api/src/main/java/me/u6k/open_jtalk/api/domain/OpenJTalkRepositoryImpl.java
@@ -42,7 +42,8 @@ public class OpenJTalkRepositoryImpl implements OpenJTalkRepository {
             File voiceDataFile = new File(workDir, voiceId.toString() + ".wav");
 
             String cmd = "open_jtalk";
-            cmd += " -m /usr/local/lib/hts_voice_nitech_jp_atr503_m001-1.05/nitech_jp_atr503_m001.htsvoice";
+            // cmd += " -m /usr/local/lib/hts_voice_nitech_jp_atr503_m001-1.05/nitech_jp_atr503_m001.htsvoice";
+            cmd += " -m /usr/local/lib/htsvoice/mei_bashful.htsvoice";
             cmd += " -x /usr/local/lib/open_jtalk_dic_utf_8-1.09/";
             if (param.getSamplingFrequency() != null) {
                 cmd += " -s " + param.getSamplingFrequency();
